fff966add81f1099bc245daf5a3a9cef6b2721da
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-08-09 21:23:01 +0100

    👋 Configured join event priority and order.

--- src/main/java/net/slqmy/tss_ranks/event/listener/ConnectionListener.java
+++ src/main/java/net/slqmy/tss_ranks/event/listener/ConnectionListener.java
@@ -5,6 +5,7 @@ import net.slqmy.tss_ranks.manager.NameTagManager;
 import net.slqmy.tss_ranks.manager.RankManager;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -20,7 +21,7 @@ public class ConnectionListener implements Listener {
 		this.plugin = plugin;
 	}
 
-	@EventHandler
+	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
 	public void onJoin(@NotNull PlayerJoinEvent event) {
 		Player player = event.getPlayer();
 		UUID playerUuid = player.getUniqueId();
