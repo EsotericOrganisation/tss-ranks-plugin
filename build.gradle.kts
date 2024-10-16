c0a313964d416c190aac509a893117e96caab5e1
Esoteric Enderman <esotericenderman@gmail.com>
2024-10-16 15:34:35 +0100

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "org.esoteric"
-version = "0.2.1"
+version = "0.2.2"
 description = "The Minecraft plugin that manages ranks and permissions on The Slimy Swamp Minecraft server."
 
 val projectNameString = rootProject.name
